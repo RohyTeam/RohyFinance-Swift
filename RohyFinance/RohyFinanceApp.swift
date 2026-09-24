10aa0ef45e5f23bb246cfd7a0b7c92697e802f00
DeeChael <DeeChael@outlook.com>
2026-09-24 09:04:24 +0800

    Initial Commit

---
+++ RohyFinance/RohyFinanceApp.swift
@@ -0,0 +1,32 @@
+//
+//  RohyFinanceApp.swift
+//  RohyFinance
+//
+//  Created by Deerio on 2026/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RohyFinanceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RohyFinanceTests/RohyFinanceTests.swift
@@ -0,0 +1,19 @@
+//
+//  RohyFinanceTests.swift
+//  RohyFinanceTests
+//
+//  Created by Deerio on 2026/9/24.
+//
+
+import Testing
+@testable import RohyFinance
+
+struct RohyFinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
